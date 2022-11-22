dc7c8656e9414bb03b8840bc58ef79e01eefaf31
Ani <83376857+Ani4557@users.noreply.github.com>
2022-11-22 09:13:46 -0800

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,1 +1,16 @@
+/*
+ * File     : decode.js
+ * Date     : 11/22/2022
+ * Author   : Ani
+*/
 
+function toDict(serialNumber) {
+  let model = serialNumber.substring(0, 5);
+  let manufacturingDate = serialNumber.substring(5, 9);
+  
+  console.log(model);
+  console.log(manufacturingDate);
+
+}
+
+toDict("0203205220021");
