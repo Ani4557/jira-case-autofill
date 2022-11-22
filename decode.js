692bdc2ff8b5acd88a1abd413ea0e09b979e92c1
Ani <83376857+Ani4557@users.noreply.github.com>
2022-11-22 09:26:04 -0800

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,14 +1,18 @@
 /*
- * File     : decode.js
- * Date     : 11/22/2022
- * Author   : Ani
+ * File    : decode.js
+ * Date    : 11/22/2022
+ * Author  : Ani
 */
 
 function toDict(serialNumber) {
   let model = serialNumber.substring(0, 5);
   let manufacturingDate = serialNumber.substring(5, 9);
-  let batch = serialNumber.substring(10, 13);
-  return {"model": model, "manufacturingDate": manufacturingDate, "batch": batch};
+  let batch = serialNumber.substring(9, 13);
+  return {
+    "model": model, 
+    "manufacturingDate": manufacturingDate, 
+    "batch": batch
+  };
 }
 
 const a = toDict("0203205220021");
