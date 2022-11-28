e4145099e3b66d70b100c43e0535e000ce3b81ac
Ani <83376857+Ani4557@users.noreply.github.com>
2022-11-28 09:06:01 -0800

    Update injector.js

--- injector.js
+++ injector.js
@@ -1,3 +1,8 @@
-Array.from(document.getElementsByClassName('Title')).forEach(el => {
+/*Array.from(document.getElementsByClassName('Title')).forEach(el => {
   el.style.setProperty('color', 'red', 'important'); 
-})
+})*/
+
+let summary = document.getElementById("summary-field");
+summary.focus();
+summary.value = "new value";
+summary.blur();
