58599204f9f6009b1c18e9d238a3fa198aceffe3
Ani <83376857+Ani4557@users.noreply.github.com>
2022-11-29 09:11:40 -0800

    Update injector.js

--- injector.js
+++ injector.js
@@ -6,3 +6,19 @@ let summary = document.getElementById("summary-field");
 summary.focus();
 summary.value = "new value";
 summary.blur();
+
+function setNativeValue(element, value) {
+  const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+  const prototype = Object.getPrototypeOf(element);
+  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+  
+  if (valueSetter && valueSetter !== prototypeValueSetter) {
+    prototypeValueSetter.call(element, value);
+  } 
+  else {
+    valueSetter.call(element, value);
+  }
+}
+
+setNativeValue(input, 'foo');
+input.dispatchEvent(new Event('input', { bubbles: true }));
