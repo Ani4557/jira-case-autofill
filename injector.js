0e8a8a40a6862c2d39c7591b254a8edb44251c93
Ani <83376857+Ani4557@users.noreply.github.com>
2022-11-29 09:20:05 -0800

    Update injector.js

--- injector.js
+++ injector.js
@@ -2,23 +2,23 @@
   el.style.setProperty('color', 'red', 'important'); 
 })*/
 
-let summary = document.getElementById("summary-field");
+/*let summary = document.getElementById("summary-field");
 summary.focus();
 summary.value = "new value";
-summary.blur();
+summary.blur();*/
 
 function setNativeValue(element, value) {
-  const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
-  const prototype = Object.getPrototypeOf(element);
-  const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
-  
-  if (valueSetter && valueSetter !== prototypeValueSetter) {
-    prototypeValueSetter.call(element, value);
-  } 
-  else {
-    valueSetter.call(element, value);
-  }
+    const valueSetter = Object.getOwnPropertyDescriptor(element, "value").set;
+    const prototype = Object.getPrototypeOf(element);
+    const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, "value").set;
+
+    if (valueSetter && valueSetter !== prototypeValueSetter) {
+        prototypeValueSetter.call(element, value);
+    } else {
+        valueSetter.call(element, value);
+    }
 }
 
-setNativeValue(input, 'foo');
-input.dispatchEvent(new Event('input', { bubbles: true }));
+setNativeValue(input, "foo");
+input.dispatchEvent(new Event("input", { bubbles: true }));
+
