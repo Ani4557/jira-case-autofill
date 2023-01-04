53da10c3895ae63713885466e72a4bca6998178d
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-03 18:27:14 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -4,3 +4,21 @@
  * Author  : Ani
  */
 
+async function getActiveTabURL() {
+  const tabs = await chrome.tabs.query({
+    currentWindow: true,
+    active: true
+  });
+  return tabs[0];
+}
+(() => {
+  const fillSerialNumberButton = document.getElementById("fill-serial-number");
+  fillSerialNumberButton.addEventListener("click", async () => {
+    const activeTab = await getActiveTabURL();
+    if (activeTab.url.includes("splitvolt.atlassian")) {
+      chrome.tabs.sendMessage(activeTab.id, {
+        action: "FILL"
+      });
+    }
+  });
+})();
