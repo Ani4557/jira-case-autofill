7eb0712f3575a442419c3b828075bb9c60869b0b
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-03 18:46:45 -0800

    Update background.js

--- background.js
+++ background.js
@@ -3,11 +3,11 @@
  * Date    : 11/22/2022
  * Author  : Ani
  */
+
 chrome.webNavigation.onCompleted.addListener((details) => {
-  if (details.frameId === 0) {
+  if(details.frameId === 0) {
     chrome.tabs.get(details.tabId, (tab) => {
-      if (tab.url === details.url && tab.url.includes("splitvolt.atlassian")) {
-        console.log("On page bg");
+      if(tab.url === details.url && tab.url.includes("splitvolt.atlassian")) {
         chrome.tabs.sendMessage(details.tabId, {
           action: "NEW"
         });
