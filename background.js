116cfff426dfe3d5a7400be35b368e7fd8627d88
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-03 18:14:58 -0800

    Update background.js

--- background.js
+++ background.js
@@ -3,16 +3,15 @@
  * Date    : 11/22/2022
  * Author  : Ani
  */
-
 chrome.webNavigation.onCompleted.addListener((details) => {
-	if (details.frameId === 0) {
-		chrome.tabs.get(details.tabId, (tab) => {
-			if (tab.url === details.url && tab.url.includes("atlassian")) {
-				console.log("On page bg");
-				chrome.tabs.sendMessage(details.tabId, {
-					action: "NEW"
-				});
-			}
-		});
-	}
+  if (details.frameId === 0) {
+    chrome.tabs.get(details.tabId, (tab) => {
+      if (tab.url === details.url && tab.url.includes("atlassian")) {
+        console.log("On page bg");
+        chrome.tabs.sendMessage(details.tabId, {
+          action: "NEW"
+        });
+      }
+    });
+  }
 });
