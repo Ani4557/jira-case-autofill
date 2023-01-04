32cf871df5efe6c4df4fb5bf6e8671e8967bc8ef
Ani <83376857+sparkra1n@users.noreply.github.com>
2023-01-03 18:13:25 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,61 +1,18 @@
 /*
- * File    : popup.js
+ * File    : background.js
  * Date    : 11/22/2022
  * Author  : Ani
  */
 
-async function getActiveTabURL() {
-  const tabs = await chrome.tabs.query({
-      currentWindow: true,
-      active: true
-  });
-
-  return tabs[0];
-}
-
-const storeSync = (key, value) => {
-  let jsonFile = {};
-  jsonFile[key] = JSON.stringify({
-    "value": value
-  });
-  chrome.storage.sync.set(jsonFile);
-}
-
-(() => {
-  const enableCheckbox = document.getElementById("enable-checkbox");
-  enableCheckbox.addEventListener("change", () => {
-      if (enableCheckbox.checked)
-        storeSync("isEnabled", 1);
-      else
-        storeSync("isEnabled", 0);
-  });
-  
-  enableCheckbox.addEventListener("onload", () => {
-
-  });
-
-  // const testButton = document.getElementById("test-button");
-  // testButton.addEventListener("click", () => {
-  //   const a = getSync();  // returns 0 or 1
-  //   alert(`returned value: ${a}`); // returns undefined
-  // });
-
-  const fillSerialNumberButton = document.getElementById("fill-serial-number");
-  fillSerialNumberButton.addEventListener("click", async () => {
-    const activeTab = await getActiveTabURL();
-    console.log(activeTab.url);
-    if (activeTab.url.includes("splitvolt.atlassian")) {
-      chrome.tabs.sendMessage(activeTab.id, {
-        action: "FILL"
-      });
-    }
-  });
-})();
-
-const getSync = () => {
-  chrome.storage.sync.get("isEnabled", (obj) => {
-    const o = JSON.parse(obj["isEnabled"]);
-    alert(`value inside function: ${o["value"]}`); // returns 0 or 1
-    return o["value"]; // returns undefined
-  });
-};
+chrome.webNavigation.onCompleted.addListener((details) => {
+	if (details.frameId === 0) {
+		chrome.tabs.get(details.tabId, (tab) => {
+			if (tab.url === details.url && tab.url.includes("atlassian")) {
+				console.log("On page bg");
+				chrome.tabs.sendMessage(details.tabId, {
+					action: "NEW"
+				});
+			}
+		});
+	}
+});
